c35302706cf648bc21b911fb278a1dedc54f4338
Marc Reichelt <marc.reichelt@iteratec.com>
2019-08-13 00:34:55 +0200

    Add initial project and talk info

---
+++ src/main/java/Talk.kt
@@ -0,0 +1,31 @@
+data class Talk(
+    val title: String,
+    val source: String,
+    val speaker: Speaker
+)
+
+data class Speaker(
+    val name: String,
+    val twitter: String,
+    val company: Company
+)
+
+data class Company(
+    val name: String,
+    val url: String
+)
+
+
+val talk = Talk(
+    "Kotlin's companion object: The power of IntelliJ IDEA",
+    "https://github.com/mreichelt/kotlin-talk-intellij-tricks",
+    Speaker("Marc Reichelt", "mreichelt", Company("iteratec GmbH", "https://iteratec.de"))
+)
+
+
+fun main() {
+    println(talk)
+    // Add names to call arguments
+    // Put arguments on separate lines
+    // `main` live template (plus arguments!)
+}
