08f974d2153221cf955a8f9b39cf0608fe977aa4
Marc Reichelt <marc.reichelt@iteratec.com>
2019-08-14 00:10:34 +0200

    Add example for Java conversion
    - also: language injection

---
+++ src/main/java/Example3Java.kt
@@ -0,0 +1,21 @@
+val someJava = """
+
+class DeepThought {
+    public static int getTheAnswerToLifeTheUniverseAndEverything() {
+        return 42;
+    }
+}
+
+""".trimIndent()
+
+fun main() {
+    println(someJava)
+    // inject language or reference
+    // add new scratch file
+    // convert Kotlin to Java
+
+//    println(DeepThought.theAnswerToLifeTheUniverseAndEverything)
+
+    // show regex example (and how to check regex!)
+    val hexColorRegex = "^#?([a-f0-9]{6}|[a-f0-9]{3})$"
+}
