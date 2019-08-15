9801fbd7305422b1fc78ef45cfd27ac9687aaee5
Marc Reichelt <marc.reichelt@iteratec.com>
2019-08-15 08:46:48 +0200

    Add more beer to thank you & add slack channel

--- src/main/java/ThankYou.kt
+++ src/main/java/ThankYou.kt
@@ -1,33 +1,40 @@
 @file:Suppress("FunctionName")
 
-val message = """
-    
+val thankYou = """
+
 .___________. __    __       ___      .__   __.  __  ___    ____    ____  ______    __    __   __  
 |           ||  |  |  |     /   \     |  \ |  | |  |/  /    \   \  /   / /  __  \  |  |  |  | |  | 
 `---|  |----`|  |__|  |    /  ^  \    |   \|  | |  '  /      \   \/   / |  |  |  | |  |  |  | |  | 
     |  |     |   __   |   /  /_\  \   |  . `  | |    <        \_    _/  |  |  |  | |  |  |  | |  | 
     |  |     |  |  |  |  /  _____  \  |  |\   | |  .  \         |  |    |  `--'  | |  `--'  | |__| 
-    |__|     |__|  |__| /__/     \__\ |__| \__| |__|\__\        |__|     \______/   \______/  (__) 
-                                                                                                   
-    
+    |__|     |__|  |__| /__/     \__\ |__| \__| |__|\__\        |__|     \______/   \______/  (__)
+
 """.trimIndent()
 
 fun main() {
-    println(message)
-
+    println(thankYou)
     `drink beer and have fun`()
     `check out the code on GitHub`()
-    `find out more IntelliJ awesomeness`()
+    `find out more awesome IntelliJ tips and tricks`()
+    `join the discussion in the intellij-tricks channel in Kotlin Slack`()
 }
 
+
 fun `drink beer and have fun`() {
-    println("🍻".repeat(10))
+    repeat(24) {
+        println("🍻".repeat(42))
+    }
 }
 
 fun `check out the code on GitHub`() {
     println("https://github.com/mreichelt/kotlin-talk-intellij-tricks")
 }
 
-fun `find out more IntelliJ awesomeness`() {
+fun `find out more awesome IntelliJ tips and tricks`() {
     println("https://github.com/mreichelt/stuff/blob/master/misc/intellij_tips.md")
+}
+
+fun `join the discussion in the intellij-tricks channel in Kotlin Slack`() {
+    println("https://kotlinlang.slack.com/messages/CM6CU9RGV")
+    // you'll need to register here if you haven't already: http://slack.kotlinlang.org/
 }
