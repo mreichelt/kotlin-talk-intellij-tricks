e7dc390f93817fd3a3cbbdb6950c2450b3bb4bb1
Marc Reichelt <marc.reichelt@iteratec.com>
2019-08-15 11:34:16 +0200

    Add second example with inline classes

--- src/main/java/Example6ReplaceWith.kt
+++ src/main/java/Example6ReplaceWith.kt
@@ -1,6 +1,10 @@
+@file:Suppress("EXPERIMENTAL_FEATURE_WARNING")
+
+
 fun oldDeprecatedLogin(password: String, username: String) {
     println("$username is logging in using old login")
 }
+
 
 fun newLogin(credentials: Credentials) {
     println("${credentials.username} is logging in")
@@ -10,6 +14,15 @@ data class Credentials(
     val username: String,
     val password: String
 )
+
+
+fun newLogin2(username: Username, password: Password) {
+    println("${username.username} is logging in with inline classes")
+}
+
+inline class Username(val username: String)
+inline class Password(val password: String)
+
 
 fun main() {
     val name = "fun main"
@@ -28,5 +41,12 @@ fun main() {
 @Deprecated(
     "use credentials class instead",
     replaceWith = ReplaceWith("newLogin(Credentials(username, password))")
+)
+ */
+
+/*
+@Deprecated(
+    "use credentials class instead",
+    replaceWith = ReplaceWith("newLogin2(Username(username), Password(password))")
 )
  */
